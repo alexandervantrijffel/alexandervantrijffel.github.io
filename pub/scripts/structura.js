fbd95e503bcb2b2c667adcc1b42bf73794b553c5
alexandervantrijffel <lexmail@gmail.com>
2014-03-09 19:55:48 +0100

    First deployable version

---
+++ pub/scripts/structura.js
@@ -0,0 +1,29 @@
+var changeYOffset;
+
+$(".showemail").click(function() {
+  var mail;
+  mail = "lexmail" + "@" + "gmail" + ".com";
+  $(this).children('span').html("<a href='mailto" + ":" + mail + "'>" + mail + "</a>");
+  $(this).attr('title', '');
+  return $(this).addClass('nolink');
+});
+
+$(".showphone").click(function() {
+  var number;
+  number = ['+', '3', '1', ' ', '(0)', '6', ' ', '1', '2', '9', ' ', '7', '4', '7', ' ', '0', '7'];
+  $(this).children('span').html("<span>" + (number.join('')) + "</span>");
+  $(this).attr('title', '');
+  return $(this).addClass('nolink');
+});
+
+$("li.social a").hover(function(inEvent) {
+  return changeYOffset($(this), 36);
+}, function(outEvent) {
+  return changeYOffset($(this), -36);
+});
+
+changeYOffset = function(element, change) {
+  var position;
+  position = /(\d+)[a-z\s]+(-\d+)/gi.exec(element.css('background-position'));
+  return element.css('background-position', "" + position[1] + "px " + (parseInt(position[2]) + change) + "px");
+};
